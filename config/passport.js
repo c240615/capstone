d2bdfc27413e496588a4d4e05176332fd26c5bbc
Claire <claire210401@gmail.com>
2023-10-19 22:33:44 +0800

    test heroku

--- config/passport.js
+++ config/passport.js
@@ -73,9 +73,9 @@ passport.use(
 passport.use(
   new GoogleStrategy(
     {
-      clientID: `${process.env.GOOGLE_ID}`,
-      clientSecret: `${process.env.GOOGLE_SECRET}`,
-      callbackURL: `${process.env.GOOGLE_CALLBACK}`,
+      clientID: `${process.env.GOOGLE_CLIENT_ID}`,
+      clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
+      callbackURL: `${process.env.GOOGLE_CLIENT_CALLBACK}`,
       profileFields: ["email", "displayName"],
     },
     (accessToken, refreshToken, profile, done) => {
